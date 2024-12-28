bd80cc7d5338d7c9f23fbeeece9e126be614652f
linda <linda.ek@bredband2.com>
2024-12-28 10:30:34 +0100

    refractoring, comments and readme

--- demo/src/main/java/ek/linda/games/guess_number.java
+++ demo/src/main/java/ek/linda/games/guess_number.java
@@ -11,11 +11,14 @@ public class guess_number implements Game{
     }
 
     String higher_or_lower(int Computer, int Guess){
-        String result = (Computer > Guess) ? "Guess is to Low\n" : "Guess is to High\n";
-        return result;
+        /**Simpel function to se if the guess is higher or lower that the actual number */
+        return (Computer > Guess) ? "Guess is to Low\n" : "Guess is to High\n";
     }
 
     String loop_for_wining(Integer computer, Integer chanses){
+        /**Function for the actual game loop. Takes in the computer number and the chanses. 
+         * continues untill the number is guessed or chanses goes to 0.
+         */
         int original_chanses = chanses;
         String player_guess;
         do{
@@ -29,14 +32,13 @@ public class guess_number implements Game{
             }
         }
         while (!Objects.equals(player_guess, computer.toString()));
-        return "Congratulations you win. You needed " + (original_chanses - chanses) + " chanses";
+    return "Congratulations you win. You needed " + (original_chanses - chanses) + " chanses";
     }
 
     int select_dificualty(){
         String[] options = {"Hard", "Medium", "Easy"};
         int chanses = 0;
-        String player_choise = general_functions.regex_spell_correct(options, general_functions.ask_the_player("Do you want to play on hard, medium or easy? "));
-        switch (player_choise) {
+        switch (general_functions.regex_spell_correct(options, general_functions.ask_the_player("Do you want to play on hard, medium or easy? "))) {
             case "Hard" -> chanses = 5;
             case "Medium" -> chanses = 10;
             case "Easy" -> chanses = 15;}
@@ -57,11 +59,14 @@ public class guess_number implements Game{
     }
 
     void guess_number_game() {
-        guess_number game = new guess_number();
+        /**This starts the game up. selects a random number for the computer and asks for a dificualty from the player.
+         * will then call the game loop.
+         * Finishes with asking for a new game regardels of win or loss.
+         */
         System.out.println("Welcome to the numbers game");
         boolean game_on = true;
         do {
-            System.out.println(game.loop_for_wining(game.computer_select_random(), game.select_dificualty()));
+            System.out.println(loop_for_wining(computer_select_random(), select_dificualty()));
             if (!general_functions.regex_find_text("Yes, yes",(general_functions.ask_the_player("Play again? Yes/No")))){
                 game_on=false;
             }
--- demo/src/test/java/ek/linda/games/hangmanTest.java
+++ demo/src/test/java/ek/linda/games/hangmanTest.java
@@ -12,4 +12,22 @@ public class hangmanTest {
         Assertions.assertEquals("___", mock);
         Assertions.assertNotEquals("____", mock);
     }
+
+    @Test
+    void reveal_test(){
+        String mock = "cat";
+        Assertions.assertTrue("_a_".equals(game.reveal_word("a", mock, "___")));
+        Assertions.assertTrue("c__".equals(game.reveal_word("c", mock, "___")));
+        Assertions.assertTrue("__t".equals(game.reveal_word("t", mock, "___")));
+        Assertions.assertFalse("__t".equals(game.reveal_word("a", mock, "___")));
+        Assertions.assertFalse("_c_".equals(game.reveal_word("c", mock, "___")));
+    }
+
+    @Test
+    void test_start(){
+        Assertions.assertEquals(6, game.game_setup("cat"));
+        Assertions.assertEquals(7, game.game_setup("lion"));
+        Assertions.assertNotEquals(7, game.game_setup("cat"));
+        Assertions.assertNotEquals(2, game.game_setup("lion"));
+    }
 }
